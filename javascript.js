bb86d85b544bedb3a8fbdff2c1300de1d80d0baa
dungdul <178075100+dungdul@users.noreply.github.com>
2026-01-12 00:53:39 +0900

    Change numbers into an array to reduce redundancy
    Instead of repeating same code with number1 and number2, now we can use
    loop to handle them.

--- javascript.js
+++ javascript.js
@@ -1,42 +1,39 @@
-let number1 = "0";
-let number2 = "0";
+let numbers = ["", ""];
 let operation = "";
 
-// To keep track if the operation is just done. If that is the case, when user presses a digit button, number1 should be overwritten, not appended
+// To keep track if the operation is just done. If that is the case, when user presses a digit button, numbers[0] should be overwritten, not appended
 let operationDone = true;
 
 const displayDiv = document.querySelector("#display");
 
 function operate() {
     // Don't do anything if operation isn't specified yet
-    // By checking only the operation, user can input a negative number. For example, by pressing - and 2 then = , number1 becomes 0 and number2 becomes 2, the result is 0 - 2 which is -2.
+    // By checking only the operation, user can input a negative number. For example, by pressing - and 2 then = , numbers[0] becomes 0 and numbers[1] becomes 2, the result is 0 - 2 which is -2.
     if (!operation) return;
 
-    number1 = +number1;
-    number2 = +number2;
+    numbers = numbers.map(number => +number);
 
     // Calculate the result
     let result;
     switch (operation) {
         case "add":
-            result = number1 + number2;
+            result = numbers[0] + numbers[1];
             break;
         case "subtract":
-            result = number1 - number2;
+            result = numbers[0] - numbers[1];
             break;
         case "multiply":
-            result = number1 * number2;
+            result = numbers[0] * numbers[1];
             break;
         case "divide":
             // Check for division by 0
-            if (number2 === 0) {
+            if (numbers[1] === 0) {
                 alert("Unfortunately, dividing by zero doesn't simply result in infinity.")
-                number1 = String(number1);
-                number2 = String(number2);
+                numbers = numbers.map(number => String(number));
                 return;
             }
 
-            result = number1 / number2;
+            result = numbers[0] / numbers[1];
             break;
     }
 
@@ -44,17 +41,17 @@ function operate() {
     result = Math.round(result * 10 ** 6) / 10 ** 6;
 
     // Update numbers and operation
-    number1 = String(result);
-    number2 = "";
+    numbers[0] = String(result);
+    numbers[1] = "";
     operation = "";
 }
 
 function updateDisplay() {
     // Update number in display panel
-    if (number2) {
-        displayDiv.textContent = number2;
-    } else if (number1) {
-        displayDiv.textContent = number1;
+    if (numbers[1]) {
+        displayDiv.textContent = numbers[1];
+    } else if (numbers[0]) {
+        displayDiv.textContent = numbers[0];
     } else {
         displayDiv.textContent = 0;
     }
@@ -69,24 +66,26 @@ function updateDisplay() {
     })
 }
 
-function updateNumber(digit) {
-    if (!operation) {
-        number1 = 
-            number1 === "0" 
-            ? digit 
-            : number1 + digit;
+function updateNumber(index, digit) {
+    if (!numbers[index] || numbers[index] === "0") {
+        if (digit === ".") {
+            numbers[index] = "0.";
+        } else {
+            numbers[index] = digit;
+        }
     } else {
-        number2 = 
-            number2 === "0"
-            ? digit
-            : number2 + digit;
+        numbers[index] += digit;
     }
 }
 
 // Update number when a digit button is pressed
 document.querySelectorAll(".digit").forEach(button => {
     button.addEventListener("click", () => {
-        updateNumber(button.textContent);
+        if (!operation) {
+            updateNumber(0, button.textContent);
+        } else {
+            updateNumber(1, button.textContent);
+        };
         updateDisplay();
     })
 })
@@ -95,7 +94,7 @@ document.querySelectorAll(".digit").forEach(button => {
 document.querySelectorAll(".operation").forEach(button => {
     button.addEventListener("click", () => {
         // Call operate() if there are already two numbers
-        if (number2) operate();
+        if (numbers[1]) operate();
 
         operation = button.id;
         updateDisplay();
@@ -104,8 +103,8 @@ document.querySelectorAll(".operation").forEach(button => {
 
 // Clear button
 document.querySelector("#clear").addEventListener("click", () => {
-    number1 = "";
-    number2 = "";
+    numbers[0] = "";
+    numbers[1] = "";
     operation = "";
     updateDisplay();
 })
